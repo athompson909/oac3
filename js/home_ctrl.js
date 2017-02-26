e49abe3752197b2ac1cbb35853aed071279161e6
athompson909 <athompson909@gmail.com>
2017-02-25 23:54:10 -0700

    changed front images

--- js/home_ctrl.js
+++ js/home_ctrl.js
@@ -6,14 +6,14 @@ app.controller('HomeCtrl', [
         $scope.jumbotronTitle = 'Outdoor Adventure Crew';
 
         $scope.advlog = {
-            title: "Grand Canyon South Rim!",
-            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/thumbnail%20images%2Fgcsr.jpg?alt=media&token=d1cf6fb2-2066-4ec1-9795-7ed271e954cd",
-            link: "blog/article/index.html?id=2"
-        };
-        $scope.gearrev = {
             title: "Yellowstone",
             image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c",
             link: "blog/article/index.html?id=1"
+        };
+        $scope.gearrev = {
+            title: "Grand Canyon South Rim!",
+            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/thumbnail%20images%2Fgcsr.jpg?alt=media&token=d1cf6fb2-2066-4ec1-9795-7ed271e954cd",
+            link: "blog/article/index.html?id=2"
         };
         $scope.survtip = {
             title: "Alpine Liesure Co. Gear Review",
