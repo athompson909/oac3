6f7eb52c0fbb019cc558d46ce6f1ff8f0ccd3f7e
athompson909 <athompson909@gmail.com>
2017-02-26 00:35:50 -0700

    added doughnut falls

--- js/home_ctrl.js
+++ js/home_ctrl.js
@@ -6,19 +6,19 @@ app.controller('HomeCtrl', [
         $scope.jumbotronTitle = 'Outdoor Adventure Crew';
 
         $scope.advlog = {
-            title: "Yellowstone",
-            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c",
-            link: "blog/article/index.html?id=1"
-        };
-        $scope.gearrev = {
             title: "Grand Canyon South Rim!",
             image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/thumbnail%20images%2Fgcsr.jpg?alt=media&token=d1cf6fb2-2066-4ec1-9795-7ed271e954cd",
             link: "blog/article/index.html?id=2"
         };
-        $scope.survtip = {
+        $scope.gearrev = {
             title: "Alpine Liesure Co. Gear Review",
             image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/thumbnail%20images%2Falcr.jpg?alt=media&token=1ca79755-8d57-4fe6-b15a-816475472b05",
             link: "gearrev/article/index.html?id=0"
+        };
+        $scope.survtip = {
+            title: "Yellowstone",
+            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c",
+            link: "blog/article/index.html?id=1"
         };
 
 
