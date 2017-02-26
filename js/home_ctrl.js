071a4ba2094513e5240efa96d9dd3aeff5974207
athompson909 <athompson909@gmail.com>
2017-02-25 19:34:51 -0700

    added gc article and modernized styling

--- js/home_ctrl.js
+++ js/home_ctrl.js
@@ -8,9 +8,9 @@ app.controller('HomeCtrl', [
         $scope.jumbotronTitle = 'Outdoor Adventure Crew';
 
         $scope.advlog = {
-            title: "Yellowstone",
-            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c",
-            link: "blog/article/index.html?id=1"
+            title: "Traveling Down",
+            image: "https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/grandcanyon%2Fthumbnail.png?alt=media&token=0b3539ed-c036-42b9-9742-a39d23937cb4",
+            link: "blog/article/index.html?id=2"
         };
         $scope.gearrev = {
             title: "Alpine Liesure Co. Gear Review",
@@ -32,3 +32,5 @@ app.controller('HomeCtrl', [
         });
     }
 ]);
+// old ys title: https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c
+// and then the more planar one: https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle.jpg?alt=media&token=836feebb-2810-4277-9b7c-dc7c16bf77a5
