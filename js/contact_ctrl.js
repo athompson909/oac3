853541ff0f359b99f2fabe39c37c52c6f95d6bac
athompson909 <athompson909@gmail.com>
2017-02-25 23:49:39 -0700

    optimizing

--- js/contact_ctrl.js
+++ js/contact_ctrl.js
@@ -1,13 +1,13 @@
 app.controller('ContactCtrl', [
     '$scope',
     function($scope) {
-      setBackgroundImage("https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/contact-us-jumbo.jpg?alt=media&token=0a6c6006-2110-4859-97f5-f7a73f2d46a9");
+      setBackgroundImage("https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/contact-us-jumbo%20(1).jpg?alt=media&token=07002b8b-e477-4f19-b310-fa006c5592fb");
     }
 ]);
 
 app.controller('PromoteCtrl', [
     '$scope',
     function($scope) {
-      setBackgroundImage("https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/contact-us-jumbo.jpg?alt=media&token=0a6c6006-2110-4859-97f5-f7a73f2d46a9");
+      setBackgroundImage("https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/contact-us-jumbo%20(1).jpg?alt=media&token=07002b8b-e477-4f19-b310-fa006c5592fb");
     }
 ]);
