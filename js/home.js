aa6217eb8bcc5ece42b00c562b76511dd65a0600
Adam Thompson <athompson909@gmail.com>
2017-03-17 10:47:55 -0600

    Update home.js
    wrong file...

--- js/home.js
+++ js/home.js
@@ -1,45 +1,52 @@
-app.controller('HomeCtrl', [
-    '$scope',
-    '$http',
-    function ($scope, $http) {
-        $scope.jumbotronWelcome = 'Sup??? welcome to...';
-        $scope.jumbotronTitle = 'Outdoor Adventure Crew';
-
-        $scope.advlog = {};
-        $scope.gearrev = {};
-        $scope.survtip = {};
-
-        $scope.getMostRecent = function() {
-            $http.get("http://localhost:8080/home?type=advlog")
-                .then(function(response) {
-                    console.log(response);
-                    $scope.advlog = response.data;
-                });
-
-            $http.get("http://localhost:8080/home?type=gearrev")
-                .then(function(response) {
-                    console.log(response);
-                    $scope.gearrev = response.data;
-                });
-
-            $http.get("http://localhost:8080/home?type=survtip")
-                .then(function(response) {
-                    console.log(response);
-                    $scope.survtip = response.data;
-                });
-        };
-
-        $scope.getMostRecent();
+function scrollToTop() {
+    // location.reload();
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
 
 
-
-        $scope.featured = [];
-        var ref2 = firebase.database().ref().child('featured');
-        ref2.once('value').then(function (obj) {
-            $scope.featured = obj.val();
-            $scope.$apply();
-        });
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
     }
-]);
-// old ys title: https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c
-// and then the more planar one: https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle.jpg?alt=media&token=836feebb-2810-4277-9b7c-dc7c16bf77a5
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function addToLink(articlesarray) {
+    console.log(articlesarray);
+    for(var i = 0; i < articlesarray.length; ++i) {
+        articlesarray[i].link = '../'+ articlesarray[i].link;
+    }
+}
+
+function add2ToLink(articlesarray) {
+    console.log(articlesarray);
+    for(var i = 0; i < articlesarray.length; ++i) {
+        articlesarray[i].link = '../../'+ articlesarray[i].link;
+    }
+}
+
+setBackgroundImage = function (imgUrl) {
+    $(".jumbotron").css("background", "url('" + imgUrl + "') no-repeat center center");
+    $(".jumbotron").css("background-size", "cover");
+}
+
+var app = angular.module('OutdoorAdvCrew', ['ngSanitize']);
+
+app.directive('dynamic', function ($compile) {
+    return {
+        restrict: 'A',
+        replace: true,
+        link: function (scope, ele, attrs) {
+            scope.$watch(attrs.dynamic, function (html) {
+                ele.html(html);
+                $compile(ele.contents())(scope);
+            });
+        }
+    };
+});
