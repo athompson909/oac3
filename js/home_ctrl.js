e2410aa8fd935d04e4d39a5ce3ee09e08dac9caa
athompson909 <athompson909@gmail.com>
2017-04-08 11:29:51 -0600

    updated featured row

--- js/home_ctrl.js
+++ js/home_ctrl.js
@@ -2,8 +2,17 @@ app.controller('HomeCtrl', [
     '$scope',
     '$window',
     function ($scope, $window) {
+        // TODO: remove
         $scope.jumbotronWelcome = 'Sup??? welcome to...';
         $scope.jumbotronTitle = 'Outdoor Adventure Crew';
+
+        $scope.changed = false;
+        $scope.wide = window.outerWidth > 740;
+        console.log($scope.wide);
+
+        $scope.thumbnailRowStyle = getThumbnailRowStyle($scope.wide);
+        $scope.thumbnailStyle = getThumbnailStyle($scope.wide);
+
 
         $scope.advlog = {
             title: "Grand Canyon South Rim!",
@@ -29,9 +38,35 @@ app.controller('HomeCtrl', [
             $scope.$apply();
         });
 
+        angular.element($window).bind('resize', function() {
+            if($window.outerWidth < 740 && $scope.wide) {
+                $scope.changed = true;
+                $scope.wide = false;
+            }
+            else if($window.outerWidth > 740 && !$scope.wide) {
+                $scope.changed = true;
+                $scope.wide = true;
+            }
+            else $scope.changed = false;
 
-        $scope.style = '';
+            if(changed) {
+                $scope.thumbnailStyle = getThumbnailStyle($scope.wide);
+                $scope.thumbnailRowStyle = getThumbnailRowStyle($scope.wide);
+                changed = false;
+            }
 
+            $scope.$digest();
+        });
+
+        function getThumbnailStyle(homeWide) {
+            return homeWide ? 'width:32%'
+                : 'width:100%;margin-bottom:-90px;';
+        }
+
+        function getThumbnailRowStyle(homeWide) {
+            return homeWide ? 'display:flex;justify-content:space-between'
+                : 'display:block;margin: 0 10%';
+        }
     }
 ]);
 // old ys title: https://firebasestorage.googleapis.com/v0/b/outdooradventurecrew-a3400.appspot.com/o/Yellowstone%2Ftitle_cropped_ys.jpg?alt=media&token=638b6cb9-7d87-4858-8f93-5d0ae331042c
