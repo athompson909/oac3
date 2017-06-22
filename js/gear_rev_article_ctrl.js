fd47c1b2536d97c9a60a3c9d9bed90f47c8b3839
athompson909 <athompson909@gmail.com>
2017-06-21 21:36:51 -0600

    finished mobile optimization

--- js/gear_rev_article_ctrl.js
+++ js/gear_rev_article_ctrl.js
@@ -1,30 +1,179 @@
+// app.controller('GearRevArticleCtrl', [
+//     '$scope',
+//     function($scope){
+//
+//       $scope.article = {};
+//       $scope.id = getParameterByName('id', null);
+//       $scope.getArticle = function() {
+//         console.log('id: '+id);
+//
+//         var ref = firebase.database().ref().child('gearReviews').child(id);
+//         ref.once('value').then(function(obj) {
+//           $scope.article = obj.val();
+//           $scope.$apply(function() {
+//             setBackgroundImage($scope.article.jumbotronImageUrl);
+//           });
+//           console.log($scope.article.images[0].orientation);
+//         });
+//       }
+//       $scope.getArticle();
+//
+//       $scope.featured = [];
+//       var ref2 = firebase.database().ref().child('featured');
+//       ref2.once('value').then(function(obj) {
+//         $scope.featured = obj.val();
+//         $scope.$apply(function() {
+//           add2ToLink($scope.featured);
+//         });
+//       });
+//     }
+// ]);
+// ===== GLOBAL PROPERTIES =====
+var lat = 37.236917,
+    lng = -113.453889,
+    id = getParameterByName('id', null);
+var site = {lat: lat, lng: lng};
+var baseUrl = "../../index.html";
+
+var titleWide = false, titleChanged = false;
+
+// ===== FACEBOOK PLUGIN =====
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '156803834800667',
+    xfbml: true,
+    version: 'v2.8'
+  });
+};
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// ===== THE CONTROLLER =====
 app.controller('GearRevArticleCtrl', [
-    '$scope',
-    function($scope){
+  '$scope',
+  '$window',
+  function ($scope, $window) {
+    window.outerWidth > 700 ? titleWide = true : titleWide = false;
+    $scope.articleType = "Our Review";
 
-      $scope.article = {};
-      $scope.id = getParameterByName('id', null);
-      $scope.getArticle = function() {
-        console.log('id: '+id);
+    $scope.baseUrl = baseUrl;
+    $scope.featured = [];
+    $scope.article = {};
+    $scope.id = getParameterByName('id', null);
 
-        var ref = firebase.database().ref().child('gearReviews').child(id);
-        ref.once('value').then(function(obj) {
-          $scope.article = obj.val();
-          $scope.$apply(function() {
-            setBackgroundImage($scope.article.jumbotronImageUrl);
-          });
-          console.log($scope.article.images[0].orientation);
-        });
-      }
-      $scope.getArticle();
+    $scope.jumboBg = '';
+    $scope.jumboTitleStyle = '';
+    $scope.jumboH1 = '';
+    $scope.jumboH2 = '';
 
-      $scope.featured = [];
+    // ===== INIT FUNCTIONS =====
+    $scope.getArticle = function () {
+
+      var ref = firebase.database().ref().child('gearReviews').child(id);
+      ref.once('value').then(function (obj) {
+        $scope.article = obj.val();
+        console.log($scope.article);
+        $scope.jumboBg = 'background: url(\''+$scope.article.jumbotronImageUrl+'\') no-repeat center center;' +
+            'background-size: cover;' +
+            'height: 28em';
+        $scope.jumboTitleStyle = 'position:relative;' +
+            'top: 50%;' +
+            'color: #fff;' +
+            'background-color: rgba(0,0,0,.4);' +
+            'font-family: \'Bree Serif\', serif';
+        $scope.jumboH1 = getH1Height();
+        $scope.jumboH2 = getH2Height();
+
+      });
+    };
+
+    $scope.getFeatured = function() {
       var ref2 = firebase.database().ref().child('featured');
-      ref2.once('value').then(function(obj) {
+      ref2.once('value').then(function (obj) {
         $scope.featured = obj.val();
-        $scope.$apply(function() {
+        $scope.$apply(function () {
           add2ToLink($scope.featured);
         });
       });
+    };
+    // ===== END =====
+
+    $scope.getArticle();
+    $scope.getFeatured();
+
+
+    angular.element($window).bind('resize', function() {
+      if($window.outerWidth < 700 && titleWide) {
+        titleChanged = true;
+        titleWide = false;
+      }
+      else if($window.outerWidth > 700 && !titleWide) {
+        titleChanged = true;
+        titleWide = true;
+      }
+      else titleChanged = false;
+
+      if(titleChanged) {
+        $scope.jumboH1 = getH1Height();
+        $scope.jumboH2 = getH2Height();
+      }
+
+      $scope.$digest();
+    });
+
+    function getH1Height() {
+      return 'font-size:'+(titleWide ?'65':'42')+'px;' +
+          'margin:0;' +
+          'padding:20px 8px 0 20px;'+
+          'line-height:'+(titleWide ?'63':'40')+'px;';
     }
+
+    function getH2Height() {
+      return 'font-size:'+(titleWide ?'30':'28')+'px;' +
+          'margin:0;' +
+          'padding:8px 8px 20px 20px;' +
+          'font-weight:normal;' +
+          'line-height:'+(titleWide ?'30':'29')+'px;';
+    }
+
+
+  }
 ]);
+
+
+// ===== ARTICLE DIRECTIVES =====
+
+app.directive('dynFbCommentBox', function () {
+  function createHTML(href, numposts, colorscheme, width) {
+    return '<div class="fb-comments" ' +
+        'data-href="' + href + '" ' +
+        'data-numposts="' + numposts + '" ' +
+        'data-colorsheme="' + colorscheme + '" ' +
+        'data-width="' + width + '">' +
+        '</div>';
+  }
+
+
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function postLink(scope, elem, attrs) {
+      attrs.$observe('pageHref', function (newValue) {
+        var href = newValue;
+        var numposts = attrs.numposts || 5;
+        var colorscheme = attrs.colorscheme || 'light';
+        var width = attrs.width || '100%';
+        elem.html(createHTML(href, numposts, colorscheme, width));
+        FB.XFBML.parse(elem[0]);
+      });
+    }
+  };
+});
---
+++ js/other_article_viewer.js
@@ -0,0 +1,102 @@
+app.directive('articleViewer', ['$window', function ($window) {
+
+    return {
+        link: link,
+        restrict: 'E',
+        template: '<div class="viewer-main main-blog" style={{mainStyle}}>' +
+                '<div id="viewer-left" style={{leftStyle}}>' +
+                    '<hr/> ' +
+                    '<div class="title-div-article">{{articleType}}</div> ' +
+                    '<hr/> ' +
+                    '<div class="article-content" dynamic="article.articleContent"></div>' +
+                    '<hr>' +
+                '</div>' +
+                '<div class="viewer-right" style={{rightStyle}}> ' +
+                    '<hr/> ' +
+                    '<div class="sidebar-title-div-bree">Featured Articles</div> ' +
+                    '<hr/> ' +
+                    '<div class="sidebar-featured"> ' +
+                        '<div ng-repeat="article in featured" class="viewer-thumbnails-right"> ' +
+                            '<article class="vw-img-cell"><a ng-href={{article.link}}><img ng-src={{article.imageSrc}}></a></article> ' +
+                            '<article class="vw-text-cell">' +
+                                '<a ng-href={{article.link}}><p class="sidebar-article-title">{{article.title}}</p></a> ' +
+                                '<p class="sidebar-article-date">{{article.date}}</p> ' +
+                            '</article>' +
+                        '</div> ' +
+                    '</div> ' +
+                    '<hr/> ' +
+                '</div>' +
+            '</div>'
+
+    };
+
+    function link(scope, element, attrs) {
+        const CHANGE_MARGIN = 880;
+        const NARROW_MARGIN = 420;
+
+        window.outerWidth > CHANGE_MARGIN ? scope.wide = true : scope.wide = false;
+        window.outerWidth < NARROW_MARGIN ? scope.narrow = true : scope.narrow = false;
+
+        scope.leftStyle = getLeftStyle();
+        scope.rightStyle = getRightStyle();
+        scope.mainStyle = getMainStyle();
+        scope.innerTitleStyle = getInnerTitleStyle();
+
+        angular.element($window).bind('resize', function() {
+            if($window.outerWidth < CHANGE_MARGIN && scope.wide) {
+                scope.changed = true;
+                scope.wide = false;
+            }
+            else if($window.outerWidth > CHANGE_MARGIN && !scope.wide) {
+                scope.changed = true;
+                scope.wide = true;
+            }
+            else scope.changed = false;
+
+            if($window.outerWidth < NARROW_MARGIN && !scope.narrow) {
+                scope.narrowChanged = true;
+                scope.narrow = true;
+            }
+            else if($window.outerWidth > NARROW_MARGIN && scope.narrow) {
+                scope.narrowChanged = true;
+                scope.narrow = false;
+            }
+            else scope.narrowChanged = false;
+
+            if(scope.changed || scope.narrowChanged) {
+                console.log('changed!!');
+                scope.leftStyle = getLeftStyle();
+                scope.rightStyle = getRightStyle();
+                scope.mainStyle = getMainStyle();
+                scope.innerTitleStyle = getInnerTitleStyle();
+            }
+
+            scope.$digest();
+        });
+
+        function getLeftStyle() {
+            return scope.wide ?
+                'width:45%;padding:3%;margin:2em 0 0 12%;' :
+                getNarrowLeftStyle();
+        }
+
+        function getNarrowLeftStyle() {
+            return scope.narrow ?
+                'width:90%;padding:5%;margin:2em 0 0 0;' :
+                'width:74%;padding:4%;margin:2em auto 0 auto;';
+        }
+
+        function getRightStyle() {
+            return scope.wide ? '' : 'display:none;';
+        }
+
+        function getMainStyle() {
+            return scope.wide ? 'display:flex;justify-content:space-around' : '';
+        }
+
+        function getInnerTitleStyle() {
+            return scope.wide ? "text-align: center;font-family: 'Permanent Marker', cursive;font-size: 54px;"
+                : "text-align: center;font-family: 'Permanent Marker', cursive;font-size: 38px;";
+        }
+    }
+}]);
